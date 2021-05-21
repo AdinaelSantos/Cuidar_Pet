290f1d08896b188a0b199468b3c6e8821f8c2140
noelAAlmeida <noelnik20@hotmail.com>
2021-05-21 18:17:16 -0300

    ajustes no cards.
    definindo as medias querys

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const sCardArticle = document.querySelector('.s-cards');
+const sCardFilho = sCardArticle.firstElementChild;
+
+console.log(sCardArticle);
+
+///sCardArticle.addEventListiner('mousehover', function(){
+
+///})
