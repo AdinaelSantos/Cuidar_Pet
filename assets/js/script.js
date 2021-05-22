9abb4395ee24190ef2fae83061645359d2089493
noelAAlmeida <noelnik20@hotmail.com>
2021-05-22 19:09:36 -0300

    criando seção infor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
-const sCardArticle = document.querySelector('.s-cards');
-const sCardFilho = sCardArticle.firstElementChild;
+/*
+    Este evento adiciona ou remove o atributo 'id' ao s-card-article.
+    mouseenter: ao entrar no s-card-article remove o 'id='diamnic-card'.
+    mouseout: ao sair do s-card-article adiciona o 'id='diamnic-card'
+*/
 
-console.log(sCardArticle);
+const sCardArticle = document.querySelectorAll('.s-cards-article');
 
-///sCardArticle.addEventListiner('mousehover', function(){
+for(let i=0;i<sCardArticle.length;i++){
+    sCardArticle[i].addEventListener('mouseenter', ()=>{
+            sCardArticle[0].removeAttribute('id');
 
-///})
+    })
+}
+for(let i=0;i<sCardArticle.length;i++){
+    sCardArticle[i].addEventListener('mouseout', ()=>{
+        sCardArticle[0].setAttribute('id', 'diamnic-card');
+    })
+}
