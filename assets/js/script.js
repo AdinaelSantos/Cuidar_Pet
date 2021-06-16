5b9e5f90e98d54a05bc7a6810260e980f7462042
noelAAlmeida <noelnik20@hotmail.com>
2021-06-16 18:14:24 -0300

    Ajustes
    Ajustes de nomes de variáves e remoção de  codigos comentados.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
+
 /*
     Este evento adiciona ou remove o atributo 'id' ao s-card-article.
     mouseenter: ao entrar no s-card-article remove o 'id='diamnic-card'.
     mouseout: ao sair do s-card-article adiciona o 'id='diamnic-card'
 */
-
 const sCardArticle = document.querySelectorAll('.s-cards-article');
 
 for(let i=0;i<sCardArticle.length;i++){
